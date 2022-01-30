cc676c44dddd7986c85f8528ace198b152fe0d92
Leo Huynh <huynhtuananh21895@gmail.com>
2022-01-30 23:31:25 +0700

    Update product page

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -15,7 +15,8 @@ export async function getServerSideProps(context) {
 }
 
 const Product = ({ product, shopInfos }) => {
-	const {moneyFormat} = shopInfos
+	console.log("=========> - product", product)
+	const { moneyFormat } = shopInfos
 	const variant = product.variants[0]
 	const sizes = product.options.find(opt => opt.name === "Size")
 	return (
@@ -28,6 +29,7 @@ const Product = ({ product, shopInfos }) => {
 							{
 								product.images.map(image => {
 									return <img
+										key={image.id}
 										src={image.src}
 										alt={product.title}
 										className="aspect-auto object-cover object-center rounded"
@@ -80,17 +82,11 @@ const Product = ({ product, shopInfos }) => {
 									{product.description}
 								</p>
 								<div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-									<div className="flex">
-										<span className="mr-3">Color</span>
-										<button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-										<button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-										<button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-									</div>
-									<div className="flex ml-6 items-center">
-										<span className="mr-3">Size</span>
+									<div className="flex items-center">
+										<span className="mr-3">Variant:</span>
 										<div className="relative">
 											<select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-												{sizes.values.map(({ value }, ind) => <option key={ind}>{value}</option>)}
+												{product.variants.map(variant => <option key={variant.id}>{formatMoney(variant.price, moneyFormat)} - {variant.title}</option>)}
 											</select>
 											<span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
 												<svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
@@ -101,8 +97,7 @@ const Product = ({ product, shopInfos }) => {
 									</div>
 								</div>
 								<div className="flex items-center">
-									<span className="title-font font-medium text-2xl text-gray-900">{formatMoney(variant.price, moneyFormat)}</span>
-									<button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy it now</button>
+									<button className="flex-1 font-medium text-center ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy it now</button>
 									<button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
 										<svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
 											<path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
