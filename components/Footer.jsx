f9a40d42b96e0aa2e68b9ad3c056caf83e0ec765
Leo Huynh <huynhtuananh21895@gmail.com>
2022-01-31 10:41:10 +0700

    Update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,19 +2,19 @@ import Link from "next/link"
 
 const Footer = () => {
 	return (
-		<footer class="fixed bottom-0 inset-x-0">
-			<div class="bg-gray-200">
-				<div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-					<p class="text-gray-500 text-sm text-center sm:text-left">© 2022 made by Leo @
-						<a href="https://leohuynh.dev" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">
+		<footer className="fixed bottom-0 inset-x-0">
+			<div className="bg-gray-200">
+				<div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+					<p className="text-gray-500 text-sm text-center sm:text-left">© 2022 made by Leo @
+						<a href="https://leohuynh.dev" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">
 							https://leohuynh.dev
 						</a>
 					</p>
-					<span class="text-sm inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-						<a href="https://twitter.com/hta218_" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">
+					<span className="text-sm inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+						<a href="https://twitter.com/hta218_" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">
 							@twitter
 						</a>
-						<a href="https://github.com/hta218" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">
+						<a href="https://github.com/hta218" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">
 							@github
 						</a>
 					</span>
