a9a60d65fd3af0d31025f0f3e903eb006b1c132a
Leo Huynh <huynhtuananh21895@gmail.com>
2022-01-31 14:54:44 +0700

    Handle buy now

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import { formatMoney } from '../../utils/format-money'
@@ -16,10 +17,22 @@ export async function getServerSideProps(context) {
 }
 
 const Product = ({ product, shopInfos }) => {
-	console.log("=========> - product", product)
 	const { moneyFormat } = shopInfos
-	const variant = product.variants[0]
-	const sizes = product.options.find(opt => opt.name === "Size")
+	const [loading, setLoading] = useState(false)
+
+	const handleByNow = async () => {
+		setLoading(true)
+		const checkout = await (await fetch('/api/checkout', {
+			method: 'POST',
+			body: JSON.stringify([{
+				variantId: product.variants[0].id,
+				quantity: 1
+			}])
+		})).json()
+		setLoading(false)
+		window.open(checkout.webUrl)
+	}
+
 	return (
 		<>
 			<Header />
@@ -98,7 +111,17 @@ const Product = ({ product, shopInfos }) => {
 									</div>
 								</div>
 								<div className="flex items-center">
-									<button className="flex-1 font-medium text-center ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy it now</button>
+									<button
+										onClick={handleByNow}
+										className={`${loading && 'pointer-events-none opacity-70'} flex-1 font-medium text-center ml-auto text-white bg-indigo-500 border-0 py-2.5 px-6 focus:outline-none hover:bg-indigo-600 rounded`}
+									>
+										{
+											loading ? <svg className="animate-spin mx-auto h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+												<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+												<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+											</svg> : <span>Buy it now</span>
+										}
+									</button>
 									<button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
 										<svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
 											<path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
